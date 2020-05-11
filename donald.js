13cc9acdafb38ff69f02989a0c53fc591482af0a
Mario Ivanovic <MrMarioI@MacBook-Pro-de-Ivanovic.local>
2020-05-11 14:47:58 +0200

    final commit

--- donald.js
+++ donald.js
@@ -2,89 +2,59 @@
 
  //const URL = "https://api.whatdoestrumpthink.com/api/v1/quotes";
 
- const list = document.getElementById("quote");
- const click = document.getElementById("button")
+ const quoteElement = document.getElementById("quote");
+ const imgElement = document.getElementById("gifs");
+ const click = document.getElementById("button");
+ const gifs = [
+     "trump1.gif",
+     "trump2.gif",
+     "trump3.gif",
+     "trump4.gif",
+     "trump5.gif",
+     "trump6.gif",
+     "trump7.gif",
+     "trump8.gif",
+     "trump9.gif",
+     "trump10.gif",
+     "trumpd.gif",
+     "giphy.gif",
+ ];
+
 
  // on va chercher la quote 
  function start() {
      axios
          .get(URL)
          .then((res) => {
-             console.log(res.data.messages)
-             display(res.data.messages);
-         })
+             // passe l'ensemble des messages non personnalisés à la fonction display ...
+             display(res.data.messages.non_personalized);
+         }) //console.log(display);
          .catch((err) => console.error(err));
-     var messages = {
-         non_personalized: [0, 49],
-         personnalized: [0, 574]
-             //console.log(messages[0]);
-     }
- };
+ }
 
- //let img = [trump1.gif, trump2.gif, trump3.gif]
+ function getRandomNumber(quotesLength) {
+     // cette fonction retourne un nombre entre 0 et quotesLength
+     return Math.floor(Math.random() * Math.floor(quotesLength));
+ }
 
- //var quotes = ['Apple', 'Banana'];
-  
- //var quotes = ['apple', 'banana', 'candies', 'dots', 'error', 'fruits'];
- //var randomQuotes = quotes[Math.floor(quotes.length * Math.random())];
-  
- // console.log(randomQuotes)
+ function getRandomGifs(gifsElementsLength) {
+     // cette fonction retourne un nombre entre 0 et quotesLength
+     return Math.floor(Math.random() * Math.floor(gifsElementsLength));
+ }
 
-
- // là je crois qu'on met le tableau dans le "HTML"
  function display(quotes) {
      // reset le HTML au cas où il y aurait qqch dans l'ul
-     list.innerHTML = "";
-
-     //appliquer le random
-     const i = Math.floor(Math.random() * quotes.length);
-     const quote = quotes[i];
-     return quotes;
-
+     const randomQuoteIndex = getRandomNumber(quotes.length);
+     const randomGifIndex = getRandomNumber(gifs.length);
+     // récup un nombre random entre 0 et la longueur de quotes
+     console.log(">>>", randomQuoteIndex);
+     console.log(quotes[randomQuoteIndex])
+     console.log(gifs[randomGifIndex])
+         //appliquer le random
+     quoteElement.innerHTML = quotes[randomQuoteIndex];
+     imgElement.src = gifs[randomGifIndex];
  }
 
- //TEST
- //const person1 = {};
- //person1['firstname'] = 'Mario';
- //person1['lastname'] = 'Rossi';
-
- //console.log(person1.firstname);
- // expected output: "Mario"
-
- //const person2 = {
- //    firstname: 'John',
- //    lastname: 'Doe'
- //};
-
- //console.log(person2['lastname']);
-
-
-
-
- function getQuotes(quotes) {
-     list.innerHTML = "";
-
-     quote.Random(quotes => {
-         console.log(quotes);
-         list.innerHTML += `<button class="btn" data-url="${quotes.url}">Random</button>
-        </li>`;
-
-     })
- }
 
 
  document.getElementById("start").onclick = start;
-
-
- // TO DO
-
- // Quote
- // Aller chercher la liste.
- // Appliquer le random.
- // Renvoyer résultat correspondant à index random.
-
- // GIFS
- // Générer index random pour gif
- // Display le gif
-
- // Quotes + gifs = onclick
